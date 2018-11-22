0fd7b6bf3054ca83df8b62900807624c1c933e28
guybi <guybiton7@gmail.com>
2018-11-22 08:00:32 +0200

    #2

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -8,7 +8,9 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class RecipeItemComponent implements OnInit {
 
   @Input() recipe: Recipe;
+
   @Input() index: number;
+  
 
   ngOnInit() {
   }
