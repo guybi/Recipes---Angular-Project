c2adc76a5efb0312a4786e4aba90770725b8832e
guybi <guybiton7@gmail.com>
2018-12-04 16:34:01 +0200

    remove AuthRoutingModule from appModule and set it in AuthModule

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,3 +1,4 @@
+import { AuthRoutingModule } from './auth-routing.module';
 import { SignupComponent } from './signup/signup.component';
 import { SigninComponent } from './signin/signin.component';
 import { NgModule } from '@angular/core';
@@ -9,7 +10,8 @@ import { FormsModule } from '@angular/forms';
         SignupComponent
     ],
     imports: [
-        FormsModule
+        FormsModule,
+        AuthRoutingModule
     ],
     exports: []
 })
